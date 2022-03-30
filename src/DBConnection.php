d6643306b1cdcc66dae0f6ea60574bd919fbe779
solarsnowfall <travis@twooutrally.com>
2022-03-29 21:04:11 -0500

    House keeping.

---
+++ src/DBConnection.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Solarsnowfall\DB;
+
+use http\Encoding\Stream\Deflate;
+use Solarsnowfall\Reflection\SingletonFactory;
+
+abstract class DBConnection extends SingletonFactory
+{
+    const CLASS_NAME = null;
+
+    /**
+     * @var MySQLConnection[]
+     */
+    protected static array $objects = [];
+
+    /**
+     * @return array
+     */
+    public static function defaultInstanceParams()
+    {
+        return include '../../../config/db.php';
+    }
+
+    /**
+     * @param array|null $params
+     * @return MySQLConnection|null
+     * @throws \Exception
+     */
+    public static function get(array $params = null): ?ConnectionInterface
+    {
+        return parent::get($params);
+    }
+
+    /**
+     * @param array $params
+     * @return MySQLConnection|null
+     * @throws \Exception
+     */
+    public static function newInstance(array $params): ?ConnectionInterface
+    {
+        return new MySQLConnection(
+            $params['host'],
+            $params['user'],
+            $params['password'],
+            $params['database']
+        );
+    }
+}
